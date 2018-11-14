d8734fb0be27f6a8999753b869683957db111284
Lokeon <kevin.lopezcala99@gmail.com>
2018-11-14 10:24:22 +0100

    :penguin:

--- app/Http/Controllers/ArticulosController.php
+++ app/Http/Controllers/ArticulosController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ArticulosController extends Controller
-{
-    //
+class ArticulosController extends Controller {
+    public function index() {
+        return view('articulos.articulos');
+    }
 }
---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+
+
+<!-- /resources/views/layout/app.blade.php -->
+<!DOCTIPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel App </title>
+</head>
+
+<body>
+    <h1>Laravel App</h1>
+    @yield('contenido')
+</body>
+</html>
